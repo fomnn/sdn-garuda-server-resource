b56658726a4d7b3db07bf9c06699d28cef2ede12
fomnn <95164774+fomnn@users.noreply.github.com>
2024-11-14 01:08:25 +0700

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,24 @@ import { account_type, attendance_status, jenis_kelamin, parent_student_relation
 import { prisma } from './db.js'
 
 async function main() {
+  // Hapus data sebelum seeding untuk menghindari konflik
+  await prisma.superadmin.deleteMany()
+  await prisma.teachers.deleteMany()
+  await prisma.classes.deleteMany()
+  await prisma.parents.deleteMany()
+  await prisma.feedbacks.deleteMany()
+  await prisma.students.deleteMany()
+  await prisma.subjects.deleteMany()
+  await prisma.class_subjects.deleteMany()
+  await prisma.parents_students.deleteMany()
+  await prisma.attendances.deleteMany()
+  await prisma.student_assignments.deleteMany()
+  await prisma.student_grades.deleteMany()
+  await prisma.subject_teachers.deleteMany()
+  await prisma.accounts.deleteMany()
+  await prisma.posts.deleteMany()
+  await prisma.posts_paragraphs.deleteMany()
+
   // Seeding data for `superadmin`
   await prisma.superadmin.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
@@ -23,13 +41,20 @@ async function main() {
     })),
   })
 
+  const teachers = await prisma.teachers.findMany()
+  const teacherIds = teachers.map(teacher => teacher.id)
+
+  
   // Seeding data for `classes`
   await prisma.classes.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
       class_name: faker.lorem.words({ min: 1, max: 4 }),
-      teacher_id: faker.number.int({ min: 1, max: 5 }),
+      teacher_id: faker.helpers.arrayElement(teacherIds),
     })),
   })
+
+  const classes = await prisma.classes.findMany()
+  const classIds = classes.map(class2 => class2.id)
 
   // Seeding data for `parents`
   await prisma.parents.createMany({
@@ -43,14 +68,17 @@ async function main() {
     })),
   })
 
+  const parents = await prisma.parents.findMany()
+  const parentIds = parents.map(parent => parent.id)
+
   // Seeding data for `feedbacks`
   await prisma.feedbacks.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
       date: faker.date.past(),
       feedback_text: faker.lorem.paragraph(),
       rating: faker.number.int({ min: 1, max: 5 }),
-      parent_id: faker.number.int({ min: 1, max: 5 }),
-      teacher_id: faker.number.int({ min: 1, max: 5 }),
+      parent_id: faker.helpers.arrayElement(parentIds),
+      teacher_id: faker.helpers.arrayElement(teacherIds),
     })),
   })
 
@@ -60,9 +88,13 @@ async function main() {
       nama: faker.person.fullName(),
       NISN: faker.string.numeric(8),
       jenis_kelamin: faker.helpers.enumValue(jenis_kelamin),
-      class_id: faker.number.int({ min: 1, max: 5 }),
+      class_id: faker.helpers.arrayElement(classIds),
     })),
   })
+
+  
+  const students = await prisma.students.findMany()
+  const studentIds = students.map(student => student.id)
 
   // Seeding data for `subjects`
   await prisma.subjects.createMany({
@@ -71,11 +103,14 @@ async function main() {
     })),
   })
 
+  const subjects = await prisma.subjects.findMany()
+  const subjectIds = subjects.map(subject => subject.id)
+
   // Seeding data for `class_subjects`
   await prisma.class_subjects.createMany({
     data: Array.from({ length: 3 }).fill(null).map(() => ({
-      class_id: faker.number.int({ min: 1, max: 5 }),
-      subject_id: faker.number.int({ min: 1, max: 5 }),
+      class_id: faker.helpers.arrayElement(classIds),
+      subject_id: faker.helpers.arrayElement(subjectIds),
     })),
   })
 
@@ -83,8 +118,8 @@ async function main() {
   await prisma.parents_students.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
       relationship: faker.helpers.enumValue(parent_student_relationships),
-      student_id: faker.number.int({ min: 1, max: 5 }),
-      parent_id: faker.number.int({ min: 1, max: 5 }),
+      student_id: faker.helpers.arrayElement(studentIds),
+      parent_id: faker.helpers.arrayElement(parentIds),
     })),
   })
 
@@ -93,8 +128,8 @@ async function main() {
     data: Array.from({ length: 5 }).fill(null).map(() => ({
       status: faker.helpers.enumValue(attendance_status),
       date: faker.date.past(),
-      student_id: faker.number.int({ min: 1, max: 5 }),
-      class_id: faker.number.int({ min: 1, max: 5 }),
+      student_id: faker.helpers.arrayElement(studentIds),
+      class_id: faker.helpers.arrayElement(classIds),
     })),
   })
 
@@ -103,35 +138,39 @@ async function main() {
       deadline_date: faker.date.soon(),
       date: new Date(faker.date.anytime().toISOString().split('T')[0]),
       title: faker.lorem.words({ min: 1, max: 6 }),
-      subject_id: faker.number.int({ min: 1, max: 5 }),
+      subject_id: faker.helpers.arrayElement(subjectIds),
     })),
   })
+
+  
+  const student_assignments = await prisma.student_assignments.findMany()
+  const studentAssignmentIds = student_assignments.map(studentAssignment => studentAssignment.id)
 
   await prisma.student_grades.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
       grade: faker.number.int({ min: 1, max: 100 }),
       term: faker.lorem.paragraphs(),
-      student_id: faker.number.int({ min: 1, max: 5 }),
-      student_assignment_id: faker.number.int({ min: 1, max: 5 }),
+      student_id: faker.helpers.arrayElement(studentIds),
+      student_assignment_id: faker.helpers.arrayElement(studentAssignmentIds),
     })),
   })
 
   await prisma.subject_teachers.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
-      subject_id: faker.number.int({ min: 1, max: 5 }),
-      teacher_id: faker.number.int({ min: 1, max: 5 }),
+      subject_id: faker.helpers.arrayElement(subjectIds),
+      teacher_id: faker.helpers.arrayElement(teacherIds),
     })),
   })
 
-  // Seeding data for `account`
-  await prisma.accounts.createMany({
-    data: Array.from({ length: 5 }).fill(null).map(() => ({
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      type: faker.helpers.enumValue(account_type),
-      user_id: faker.number.int({ min: 1, max: 5 }),
-    })),
-  })
+  // // Seeding data for `account`
+  // await prisma.accounts.createMany({
+  //   data: Array.from({ length: 5 }).fill(null).map(() => ({
+  //     email: faker.internet.email(),
+  //     password: faker.internet.password(),
+  //     type: faker.helpers.enumValue(account_type),
+  //     user_id: faker.number.int({ min: 1, max: 5 }),
+  //   })),
+  // })
 
   await prisma.posts.createMany({
     data: Array.from({ length: 5 }).fill(null).map(() => ({
@@ -139,15 +178,17 @@ async function main() {
     })),
   })
 
+  const posts = await prisma.posts.findMany()
+  const postIds = posts.map(post => post.id)
+
+
   await prisma.posts_paragraphs.createMany({
     data: Array.from({ length: 15 }).fill(null).map(() => ({
       content: faker.lorem.paragraph(),
       paragraph_order: faker.number.int({ min: 1, max: 150 }),
-      post_id: faker.number.int({ min: 1, max: 5 }),
+      post_id: faker.helpers.arrayElement(postIds),
     })),
   })
-
-  
 }
 
 main()
